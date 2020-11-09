f3dd82d6086b741461d9c1e7094d750d30d51569
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-11-09 10:06:43 +0800

    Remove alias and functions

--- src/index.sh
+++ src/index.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # import all from src/
-# manually import functions and alias for more control
 
 BASH_STYLES_PATH="$HOME/.bash_styles/"
 
@@ -9,6 +8,4 @@ source "${BASH_STYLES_PATH}colors.sh"
 source "${BASH_STYLES_PATH}texts.sh"
 source "${BASH_STYLES_PATH}header.sh"
 source "${BASH_STYLES_PATH}prompt.sh"
-# source "${BASH_STYLES_PATH}functions.sh"
-# source "${BASH_STYLES_PATH}alias.sh"
 source "${BASH_STYLES_PATH}clearvar.sh"
