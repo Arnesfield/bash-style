de73ca8d8eeadcae6b5de624689e1a15b1bc993a
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-11-09 10:13:44 +0800

    Add noparen argument for git branch by default

--- src/prompt.sh
+++ src/prompt.sh
@@ -16,6 +16,6 @@ fi
 
 PS1="\n$STYL_CLR_USER$STYL_PS1_USER$STYL_CLR_DEF" # user@host
 PS1="$PS1 $STYL_CLR_DIR\W" # dir
-PS1="$PS1$STYL_CLR_DEF $STYL_CLR_BRANCH\$(parse_git_branch)" # git branch
+PS1="$PS1$STYL_CLR_DEF $STYL_CLR_BRANCH\$(parse_git_branch noparen)" # git branch
 PS1="$PS1$STYL_CLR_DEF\n:: $STYL_CLR_TEXT\$(styl_rand_text)" # text
 PS1="$PS1$STYL_CLR_DEF\n$ " # input
